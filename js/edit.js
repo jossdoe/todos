65d06e552f0c6284a5ddf4e9a0e898516d479639
Joss Doebler <mail@joss-doebler.de>
2020-07-15 14:07:07 +0200

    Refactored JS a bit to make more readable

--- js/edit.js
+++ js/edit.js
@@ -1,38 +1,30 @@
 // # EVENT LISTENERS
 // Toggle Checked for all Todo icons
 document.querySelectorAll('div.todo-icon').forEach((el) => {
-  el.addEventListener('click', () => toggleChecked(el));
+  el.addEventListener('click', () => toggleChecked(el.parentElement));
 });
 
 // Toggle Edit Mode for all Todo descriptions
 document.querySelectorAll('.todo-description').forEach((el) => {
-  el.addEventListener('click', () => toggleEditMode(el));
+  el.addEventListener('click', () => toggleEditMode(el.parentElement));
 });
 
 // Toggle Delete Mode for all Todo descriptions
 document.querySelectorAll('.todo-description').forEach((el) => {
-  el.addEventListener('click', () => toggleDeleteMode(el));
+  el.addEventListener('click', () => toggleDeleteMode(el.parentElement));
 });
 
 // # FUNCTIONS
-function toggleChecked(iconElement) {
-  const todoElement = iconElement.parentElement;
-
-  if (todoElement.classList.contains('todo-checked')) {
-    todoElement.classList.remove('todo-checked');
-    todoElement.classList.add('todo-unchecked');
-  } else {
-    todoElement.classList.remove('todo-unchecked');
-    todoElement.classList.add('todo-checked');
-  }
+function toggleChecked(todoElement) {
+  todoElement.classList.toggle('todo-checked');
+  todoElement.classList.toggle('todo-unchecked');
 }
 
-function toggleEditMode(textElement) {
-  const todoElement = textElement.parentElement;
+function toggleEditMode(todoElement) {
+  const description = todoElement.querySelector('.todo-description').innerHTML;
+
   // We don't want to activate edit mode on checked off items
   if (todoElement.classList.contains('todo-checked')) return;
-
-  const description = textElement.innerHTML;
 
   const editField = document.createElement('input');
   editField.type = 'text';
@@ -56,16 +48,18 @@ function saveEdit(todoElement) {
   const elTodoIcon = document.createElement('div');
   elTodoIcon.classList.add('todo-icon');
   elTodoIcon.innerHTML = '<img src="assets/checkmark.svg" alt="Checkmark" />';
-  elTodoIcon.addEventListener('click', () => toggleChecked(elTodoIcon));
+  elTodoIcon.addEventListener('click', () =>
+    toggleChecked(elTodoIcon.parentElement)
+  );
 
   const elTodoDescription = document.createElement('div');
   elTodoDescription.classList.add('todo-description');
   elTodoDescription.innerHTML = inputValue;
   elTodoDescription.addEventListener('click', () =>
-    toggleEditMode(elTodoDescription)
+    toggleEditMode(elTodoDescription.parentElement)
   );
   elTodoDescription.addEventListener('click', () =>
-    toggleDeleteMode(elTodoDescription)
+    toggleDeleteMode(elTodoDescription.parentElement)
   );
 
   const elTrashIcon = document.createElement('div');
@@ -78,11 +72,8 @@ function saveEdit(todoElement) {
   todoElement.append(elTrashIcon);
 }
 
-function toggleDeleteMode(textElement) {
-  const todoElement = textElement.parentElement;
-  console.log(textElement.parentElement);
+function toggleDeleteMode(todoElement) {
   // We don't want to activate delete mode on active items
-  if (todoElement.classList.contains('todo-unchecked')) return;
-
-  todoElement.classList.toggle('expand');
+  if (todoElement && todoElement.classList.contains('todo-checked'))
+    todoElement.classList.toggle('expand');
 }
