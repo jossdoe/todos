29e0bc6c5d708923f51eacf0160f73bef7db3a2f
Joss Doebler <mail@joss-doebler.de>
2020-07-15 12:14:15 +0200

    Moved all edit functions into one file

--- js/edit.js
+++ js/edit.js
@@ -1,3 +1,21 @@
+// Toggle checked status of Todo
+const toggleChecked = (iconElement) => {
+  const todoElement = iconElement.parentElement;
+
+  if (todoElement.classList.contains('todo-checked')) {
+    todoElement.classList.remove('todo-checked');
+    todoElement.classList.add('todo-unchecked');
+  } else {
+    todoElement.classList.remove('todo-unchecked');
+    todoElement.classList.add('todo-checked');
+  }
+};
+
+// Event Listener for all icon elements on Todos
+document.querySelectorAll('div.todo-icon').forEach((el) => {
+  el.addEventListener('click', () => toggleChecked(el));
+});
+
 // Replace content of Todo-element with field & button
 function toggleEditMode(textElement) {
   const description = textElement.innerHTML;
